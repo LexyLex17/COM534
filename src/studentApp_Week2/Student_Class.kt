001871d4f48e6cc9da0254ea0326c6d5af6be59e
Gus <2jonea44@solent.ac.uk>
2024-10-13 18:54:29 +0100

    Student bit

--- src/studentApp_Week2/Student_Class.kt
+++ src/studentApp_Week2/Student_Class.kt
@@ -1,4 +1,22 @@
 package studentApp_Week2
 
-class Student_Class {
+class Student_Class(idIn: String, nameIn: String, courseIn: String, markIn: Double?) {
+
+    // Attributes
+    val id: String
+    val name: String
+    val course: String
+    val mark: Double?
+
+    init {
+        id = idIn
+        name = nameIn
+        course = courseIn
+        mark = markIn
+    }
+
+    override fun toString() : String {
+        return "Name: $name, Course: $course, Mark: $mark"
+    }
+
 }
--- src/studentApp_Week2/Student_Main.kt
+++ src/studentApp_Week2/Student_Main.kt
@@ -1,2 +1,19 @@
 package studentApp_Week2
 
+fun main() {
+    for (i in 1..3) {
+        println("Enter the student's ID:")
+        val idIn: String = readLine().toString()
+        println("Enter the student's name:")
+        val nameIn = readLine().toString()
+        println("Enter the student's course:")
+        val courseIn = readLine().toString()
+        println("Enter the student's mark:")
+        val markIn: Double? = readLine()?.toDouble()
+        val student = Student_Class(idIn, nameIn, courseIn, markIn)
+        println("$student")
+    }
+}
+
+
+
